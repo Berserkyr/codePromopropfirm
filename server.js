c218cc6ab896dc33d999f155c197e6ad69cd82e4
Nicolas Draperi <nicolas.draperi2@gmail.com>
2024-10-25 16:30:35 +0200

    fix server.js

--- server.js
+++ server.js
@@ -9,17 +9,14 @@ app.use(express.json());
 app.use(express.static('public'));
 //app.use(express.urlencoded({ extended: true }));
 
-//route codepromo
-app.use('/api/promocodes', codeRoutes);
 const codeRoutes = require('./routes/codeRoutes'); 
-
-//route propfirm
 const propfirmRoutes = require('./routes/propfirmRoutes'); 
-app.use('/api/propfirm', propfirmRoutes); 
-
-//route newsletter
 const newsletterRoutes = require('./routes/newsletterRoutes'); 
+app.use('/api/propfirm', propfirmRoutes); 
+app.use('/api/promocodes', codeRoutes);
 app.use('/api/newsletter', newsletterRoutes);
+
+
 
 const PORT = process.env.PORT || 3001;
 const HOST = "0.0.0.0"
